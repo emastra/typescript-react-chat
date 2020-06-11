ba58947e01a422fe63b31ac73e2bac724b941f68
emastra <emiliano.mastra@gmail.com>
2020-06-11 22:36:44 +0200

    route for all routes added

--- src/server/index.js
+++ src/server/index.js
@@ -11,20 +11,28 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../../dist')));
 
-// test middleware
-app.use(function (req, res, next) {
-    console.log(req.method, req.url, req.params, req.query);
-    next();
+// // test middleware
+// app.use((req, res, next) => {
+//     console.log(req.method, req.url, req.params, req.query);
+//     next();
+// });
+
+// all routes route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../dist/index.html'));
 });
 
-
+// socket on connection
 io.on('connection', (socket) => {
     console.log('New user connected. Socket:', socket.id);
 
+    // event listener for createMessage
     socket.on('createMessage', (message, callback) => {
+        // emit the received message
         io.emit('newMessage', message);
     });
 
+    // on disconnect
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
